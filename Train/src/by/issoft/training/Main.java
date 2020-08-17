1d4c120e24e9e4a44b5529dfbeeef45025baea0f
Valeria-Sem <lerasemenenya@gmail.com>
2020-08-17 15:20:29 +0300

    some changes

--- Train/src/by/issoft/training/Main.java
+++ Train/src/by/issoft/training/Main.java
@@ -15,15 +15,15 @@ public class Main {
         Cargo cargo5 = new Cargo("concrete", 555.6, 1);
         Cargo cargo6 = new Cargo("glass", 100, 1);
 
-        RailwayCarriage railwayCarriage1 = new RailwayCarriage(400);
-        RailwayCarriage railwayCarriage2 = new RailwayCarriage(200);
-        RailwayCarriage railwayCarriage3 = new RailwayCarriage(600);
-        RailwayCarriage railwayCarriage4 = new RailwayCarriage(800);
+        RailwayCarriage railwayCarriage1 = new RailwayCarriage(400, 1);
+        RailwayCarriage railwayCarriage2 = new RailwayCarriage(200, 2);
+        RailwayCarriage railwayCarriage3 = new RailwayCarriage(600, 3);
+        RailwayCarriage railwayCarriage4 = new RailwayCarriage(800, 4);
 
-        railwayCarriage1.counter(cargo1, cargo6);
-        railwayCarriage2.counter(cargo2);
-        railwayCarriage3.counter(cargo4, cargo3);
-        railwayCarriage4.counter(cargo5);
+        railwayCarriage1.loadingCargo(cargo1, cargo6);
+        railwayCarriage2.loadingCargo(cargo2);
+        railwayCarriage3.loadingCargo(cargo4, cargo3);
+        railwayCarriage4.loadingCargo(cargo5);
 
         Train train = new Train();
         train.addRailwayCarriage(railwayCarriage1, railwayCarriage2, railwayCarriage3, railwayCarriage4);
--- Train/src/by/issoft/training/core/RailwayCarriage.java
+++ Train/src/by/issoft/training/core/RailwayCarriage.java
@@ -3,9 +3,11 @@ package by.issoft.training.core;
 public class RailwayCarriage {
 
     private double carryingCapacity;
+    private int railwayCarriageNumber;
 
-    public RailwayCarriage(double carryingCapacity) {
+    public RailwayCarriage(double carryingCapacity, int railwayCarriageNumber) {
         this.carryingCapacity = carryingCapacity;
+        this.railwayCarriageNumber = railwayCarriageNumber;
     }
 
     public double getCarryingCapacity() {
@@ -16,18 +18,29 @@ public class RailwayCarriage {
         this.carryingCapacity = carryingCapacity;
     }
 
+    public int getRailwayCarriageNumber() {
+        return railwayCarriageNumber;
+    }
+
+    public void setRailwayCarriageNumber(int railwayCarriageNumber) {
+        this.railwayCarriageNumber = railwayCarriageNumber;
+    }
+
+
     private  Cargo[] cargos = new Cargo[100];
 
-    public void counter(Cargo... cargos){
+    public void loadingCargo(Cargo... cargos){
         for (int i=0; i < cargos.length; i++){
             if(cargos[i].getTotalWeight() > carryingCapacity){
                 throw new RuntimeException("Cargos: " + cargos[i].getDescription() + " with total weight = " +
-                        cargos[i].getTotalWeight() + " too heavy for this railway carriage");
+                        cargos[i].getTotalWeight() + " too heavy for this railway carriage - " + railwayCarriageNumber);
             }
-            if (carryingCapacity > 0 && carryingCapacity - cargos[i].getTotalWeight() >= 0){
+            System.out.println("Cargos: " + cargos[i].getDescription() + " with total weight = " +
+                    cargos[i].getTotalWeight() + " was loaded into railway carriage № " + railwayCarriageNumber +
+                    " with carrying capacity = " + carryingCapacity);
                 this.cargos[i] = cargos[i];
                 carryingCapacity -= cargos[i].getTotalWeight();
-            }
+                System.out.println("Now carrying capacity = " + carryingCapacity);
         }
     }
 }
